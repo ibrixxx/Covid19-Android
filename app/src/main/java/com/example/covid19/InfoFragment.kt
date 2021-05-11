5774247c0e972979508c41a8bdaccca7f200d738
ibrahim <ibrahinmesan@gmail.com>
2021-05-11 14:34:23 +0200

    Final

--- app/src/main/java/com/example/covid19/InfoFragment.kt
+++ app/src/main/java/com/example/covid19/InfoFragment.kt
@@ -1,34 +1,53 @@
 package com.example.covid19
 
-import android.app.Activity
+
 import android.content.res.Configuration
 import android.content.res.Resources
 import android.os.Bundle
-import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.appcompat.app.AppCompatActivity
 import androidx.databinding.DataBindingUtil
-import androidx.navigation.ui.NavigationUI
-import com.example.covid19.databinding.ActivityMainBinding
+import androidx.fragment.app.Fragment
+import androidx.fragment.app.FragmentActivity
 import com.example.covid19.databinding.FragmentInfoBinding
-import com.example.covid19.databinding.FragmentStatsBinding
 import java.util.*
 
+
 class InfoFragment : Fragment() {
-    override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?,
-                              savedInstanceState: Bundle?): View? {
-        val binding = DataBindingUtil.inflate<FragmentInfoBinding>(inflater, R.layout.fragment_info, container, false)
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        val binding = DataBindingUtil.inflate<FragmentInfoBinding>(
+            inflater,
+            R.layout.fragment_info,
+            container,
+            false
+        )
         (activity as AppCompatActivity?)!!.supportActionBar!!.title = getString(R.string.infoTitle)
+
+        binding.buttonEng.setOnClickListener {
+            setLocale(activity, "en")
+            binding.textView3.text = resources.getString(R.string.ChoseLang)
+            (activity as AppCompatActivity?)!!.supportActionBar!!.title = getString(R.string.infoTitle)
+        }
+
+        binding.buttonBa.setOnClickListener {
+            setLocale(activity, "bs")
+            binding.textView3.text = resources.getString(R.string.ChoseLang)
+            (activity as AppCompatActivity?)!!.supportActionBar!!.title = getString(R.string.infoTitle)
+        }
 
         return binding.root
     }
 
-    fun setLocale(activity: Activity, languageCode: String?) {
+    fun setLocale(activity: FragmentActivity?, languageCode: String?) {
         val locale = Locale(languageCode)
         Locale.setDefault(locale)
-        val resources: Resources = activity.resources
+        val resources: Resources = requireActivity().resources
         val config: Configuration = resources.getConfiguration()
         config.setLocale(locale)
         resources.updateConfiguration(config, resources.getDisplayMetrics())
